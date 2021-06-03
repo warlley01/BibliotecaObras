5d098fdc9a56276eff79651ec2910c63f3f61a30
warlley01 <warlleyjhones@gmail.com>
2021-06-03 13:15:52 -0400

    Adicionado Comentários

--- Biblioteca de Obras/baseDB.py
+++ Biblioteca de Obras/baseDB.py
@@ -1,15 +1,19 @@
 import sqlite3
 
+# conectar ao banco
 con = sqlite3.connect('base.db')
 
+# cursor para executar comandos no banco
 cur = con.cursor()
 
+# comando sql
 sql = """
         CREATE TABLE obras (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                             titulo TEXT NOT NULL,
                             sinopse TEXT NOT NULL)
       """
       
+# executar sql
 cur.execute(sql)
 con.commit()
 con.close()      
--- Biblioteca de Obras/src/cgi-bin/addObra.py
+++ Biblioteca de Obras/src/cgi-bin/addObra.py
@@ -1,21 +1,27 @@
 import sqlite3
 import cgi
 
+# Pegar Valor do Form
 formValue = cgi.FieldStorage()
 
+# o parametro de "getvalue()" é o name do input no HTML 
 titulo = formValue.getvalue('tituloInput')
 sinopse = formValue.getvalue('sinopseInput')
 
+# Conectar ao banco de dados 
 con = sqlite3.connect('../base.db')
 
+# cursor para executar comandos no banco
 cur = con.cursor()
 
+# Função que retorna um simples comando sql com os dados que pegamos do form
 def insertObra(tituloParam, sinopseParam):
     return """
             INSERT INTO obras(titulo, sinopse)
                 VALUES('{}', '{}')
            """.format(tituloParam, sinopseParam)
            
+# executa o sql da função no banco de dados
 cur.execute(insertObra(titulo, sinopse))
 
 con.commit()
